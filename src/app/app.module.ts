8178a5b92c7ef4f89de9162508f279f374a4336f
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-02-04 17:08:02 +0100

    install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SceneModule } from './scene/scene.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SceneModule],
+  imports: [BrowserModule, AppRoutingModule, SceneModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
